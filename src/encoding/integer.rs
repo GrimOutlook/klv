a1517675d033b62645eaf8127949fef252e8f9dd
Dominic Grimaldi <self@grimoutlook.dev>
2026-01-21 21:41:13 -0600

    refactor: Changed Integer to SignedInteger

--- src/encoding/integer.rs
+++ src/encoding/integer.rs
@@ -9,9 +9,9 @@ use std::io::Read;
 
 use crate::encoding::Error;
 
-/// Integer types that can be read in using `read_integer`.
+/// Integer types that can be read in using `read_signed_integer`.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Integer {
+pub enum SignedInteger {
     I8(i8),
     I16(i16),
     I32(i32),
@@ -19,43 +19,43 @@ pub enum Integer {
     I128(i128),
 }
 
-/// Read in a variable length integer.
+/// Read in a variable length signed integer.
 ///
-/// Integers can be stored in variable lengths that adjust based on their
-/// numeric values. Successfully parsed integers are always returned in the
-/// smallest datatype the number of bytes read in fits in (e.g., 3 bytes will
-/// always return an `i32`).
+/// Signed integers can be stored in variable lengths that adjust based on their
+/// numeric values. Successfully parsed signed integers are always returned in
+/// the smallest datatype the number of bytes read in fits in (e.g., 3 bytes
+/// will always return an `i32`).
 ///
 /// # Args
 ///
 /// - `buf` - Buffer to read from.
-/// - `length` - Number of bytes to read and interpret as an integer.
+/// - `length` - Number of bytes to read and interpret as a signed integer.
 ///
 /// # Returns
 ///
-/// - `Ok(Integer)` - Number of bytes read in from buffer can successfully be
-///   interpreted as an i128 or smaller datatype.
-/// - `Err(encoding::Error)` - Number of bytes read cannot fit into an integer
-///   container, number of bytes to read is zero, or there is an error reading
-///   from the buffer.
+/// - `Ok(SignedInteger)` - Number of bytes read in from buffer can successfully
+///   be interpreted as an i128 or smaller datatype.
+/// - `Err(encoding::Error)` - Number of bytes read cannot fit into a signed
+///   integer container, number of bytes to read is zero, or there is an error
+///   reading from the buffer.
 ///
 /// # Side Effects
 ///
 /// Moves the current position in the buffer to the byte after the last byte
 /// read
-pub fn read_integer<T>(buf: &mut T, length: u8) -> Result<Integer, Error>
+pub fn read_signed_integer<T>(buf: &mut T, length: u8) -> Result<SignedInteger, Error>
 where
     T: Read + Seek,
 {
     let value = match length {
-        1 => Integer::I8(buf.read_i8()?),
-        2 => Integer::I16(buf.read_i16::<byteorder::BigEndian>()?),
-        3 | 4 => Integer::I32(
+        1 => SignedInteger::I8(buf.read_i8()?),
+        2 => SignedInteger::I16(buf.read_i16::<byteorder::BigEndian>()?),
+        3 | 4 => SignedInteger::I32(
             buf.read_int::<byteorder::BigEndian>(length as usize)?
                 .try_into()
                 .unwrap_or_else(|_| panic!("{length} bytes doesn't fit in an `i32` somehow")),
         ),
-        5..=8 => Integer::I64(buf.read_int::<byteorder::BigEndian>(length as usize)?),
+        5..=8 => SignedInteger::I64(buf.read_int::<byteorder::BigEndian>(length as usize)?),
         9..=16 => {
             let bytes = (0..length)
                 .map(|_| buf.read_u8().map_err(Error::from))
@@ -75,9 +75,9 @@ where
                 *bits_mut.first_mut().unwrap() = true
             }
 
-            Integer::I128(bits_mut.load_be())
+            SignedInteger::I128(bits_mut.load_be())
         }
-        _ => return Err(Error::DecodingError("integer".to_string())),
+        _ => return Err(Error::DecodingError("signed_integer".to_string())),
     };
 
     Ok(value)
@@ -104,7 +104,7 @@ where
     Ok(buf.read_i8()?)
 }
 
-/// Reads 2 bytes and interpresets it as an `i16` in `BigEndian` format.
+/// Reads 2 bytes and interprets it as an `i16` in `BigEndian` format.
 ///
 /// This is just a wrapper around `byteorder::ReadBytesExt::read_i16` provided
 /// for convenience.
@@ -195,24 +195,24 @@ mod tests {
     use super::*;
     use test_case::test_case;
 
-    #[test_case(&[0x00], Integer::I8(0); "i8 Zero")]
-    #[test_case(&[0x80], Integer::I8(i8::MIN); "i8 Min")]
-    #[test_case(&[0x7F], Integer::I8(i8::MAX); "i8 Max")]
-    #[test_case(&[0x00, 0x00], Integer::I16(0); "i16 Zero")]
-    #[test_case(&[0x80, 0x00], Integer::I16(i16::MIN); "i16 Min")]
-    #[test_case(&[0x7F, 0xFF], Integer::I16(i16::MAX); "i16 Max")]
-    #[test_case(&[0x00, 0x00, 0x00, 0x00], Integer::I32(0); "i32 Zero")]
-    #[test_case(&[0x80, 0x00, 0x00, 0x00], Integer::I32(i32::MIN); "i32 Min")]
-    #[test_case(&[0x7F, 0xFF, 0xFF, 0xFF], Integer::I32(i32::MAX); "i32 Max")]
-    #[test_case(&[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], Integer::I64(0); "i64 Zero")]
-    #[test_case(&[0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], Integer::I64(i64::MIN); "i64 Min")]
-    #[test_case(&[0x7F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], Integer::I64(i64::MAX); "i64 Max")]
-    #[test_case(&[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], Integer::I128(0); "i128 Zero")]
-    #[test_case(&[0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], Integer::I128(i128::MIN); "i128 Min")]
-    #[test_case(&[0x7F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], Integer::I128(i128::MAX); "i128 Max")]
-    fn read_integer_ok(input: &[u8], expected: Integer) {
+    #[test_case(&[0x00], SignedInteger::I8(0); "i8 Zero")]
+    #[test_case(&[0x80], SignedInteger::I8(i8::MIN); "i8 Min")]
+    #[test_case(&[0x7F], SignedInteger::I8(i8::MAX); "i8 Max")]
+    #[test_case(&[0x00, 0x00], SignedInteger::I16(0); "i16 Zero")]
+    #[test_case(&[0x80, 0x00], SignedInteger::I16(i16::MIN); "i16 Min")]
+    #[test_case(&[0x7F, 0xFF], SignedInteger::I16(i16::MAX); "i16 Max")]
+    #[test_case(&[0x00, 0x00, 0x00, 0x00], SignedInteger::I32(0); "i32 Zero")]
+    #[test_case(&[0x80, 0x00, 0x00, 0x00], SignedInteger::I32(i32::MIN); "i32 Min")]
+    #[test_case(&[0x7F, 0xFF, 0xFF, 0xFF], SignedInteger::I32(i32::MAX); "i32 Max")]
+    #[test_case(&[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], SignedInteger::I64(0); "i64 Zero")]
+    #[test_case(&[0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], SignedInteger::I64(i64::MIN); "i64 Min")]
+    #[test_case(&[0x7F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], SignedInteger::I64(i64::MAX); "i64 Max")]
+    #[test_case(&[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], SignedInteger::I128(0); "i128 Zero")]
+    #[test_case(&[0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], SignedInteger::I128(i128::MIN); "i128 Min")]
+    #[test_case(&[0x7F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], SignedInteger::I128(i128::MAX); "i128 Max")]
+    fn read_signed_integer_ok(input: &[u8], expected: SignedInteger) {
         assert_eq!(
-            read_integer(
+            read_signed_integer(
                 &mut std::io::Cursor::new(input),
                 input.len().try_into().unwrap()
             )
@@ -221,20 +221,20 @@ mod tests {
         );
     }
 
-    #[test_case( &[], io::Error::from(io::ErrorKind::UnexpectedEof); "Integer buffer has no bytes")]
-    #[test_case( &[0x81], io::Error::from(io::ErrorKind::UnexpectedEof); "Integer buffer ends unexpectedly")]
-    fn read_integer_io_err(input: &[u8], expected: io::Error) {
-        let err = read_integer(&mut std::io::Cursor::new(input), 2)
+    #[test_case( &[], io::Error::from(io::ErrorKind::UnexpectedEof); "Signed Integer buffer has no bytes")]
+    #[test_case( &[0x81], io::Error::from(io::ErrorKind::UnexpectedEof); "Signed Integer buffer ends unexpectedly")]
+    fn read_signed_integer_io_err(input: &[u8], expected: io::Error) {
+        let err = read_signed_integer(&mut std::io::Cursor::new(input), 2)
             .expect_err("Testcase should fail here but does not")
             .try_as_other()
             .unwrap();
         assert_eq!(err.kind(), expected.kind())
     }
 
-    #[test_case( &[0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F], 17, Error::DecodingError("integer".to_string()); "Length too long")]
-    #[test_case( &[0x00], 0, Error::DecodingError("integer".to_string()); "Length to read is zero")]
-    fn read_integer_decoding_err(input: &[u8], length: u8, expected: Error) {
-        let err = read_integer(&mut std::io::Cursor::new(input), length)
+    #[test_case( &[0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F], 17, Error::DecodingError("signed_integer".to_string()); "Length too long")]
+    #[test_case( &[0x00], 0, Error::DecodingError("signed_integer".to_string()); "Length to read is zero")]
+    fn read_signed_integer_decoding_err(input: &[u8], length: u8, expected: Error) {
+        let err = read_signed_integer(&mut std::io::Cursor::new(input), length)
             .expect_err("Testcase should fail here but does not");
         assert_eq!(err.to_string(), expected.to_string())
     }
--- src/encoding/mod.rs
+++ src/encoding/mod.rs
@@ -1,23 +1,56 @@
 use std::io;
 
+use crate::encoding::{integer::SignedInteger, unsigned_integer::UnsignedInteger};
+
 pub mod ber;
 pub mod ber_oid;
 pub mod integer;
 pub mod unsigned_integer;
 
-pub trait EnumerationValue {}
-
 /// Values enumerated here are copied from _Table 40_ on page 115 of
 /// _MISP-2025.1: Motion Imagery Handbook_
+#[derive(Clone, Debug, strum::EnumDiscriminants)]
 pub enum SimpleDataType {
-    Ber,
-    BerOid,
-    Binary,
-    Boolean,
-    Iso7,
-    Utf8,
+    Ber(u128),
+    BerOid(u128),
+
+    /// A Binary data type compacts a set of information into one or more bytes.
+    /// The binary type can contain flags, small enumerations and other
+    /// bit-specified controls. When MISP metadata standards use binary types,
+    /// the standard describes how to interpret each bit of the binary value
+    Binary(Vec<u8>),
+
+    /// With a byte, when the byte value equals zero (0x00), the Boolean meaning
+    /// is false; when the byte value is one (0x01) the Boolean meaning it true;
+    /// all other values are not valid.
+    Boolean(bool),
+
+    /// The ISO7 data type is one or more characters from the ISO/IEC 646:1991
+    /// [19] character set. ISO7 uses the lower seven bits of a byte; the eighth
+    /// bit is always zero.
+    ///
+    /// WARN: ISO7 supports the Latin character set for English only. For this
+    /// reason, the MISP is discontinuing ISO7 in document updates and new
+    /// publications. Some existing MISP standards use ISO7 for historical and
+    /// backward compatibility reasons. The replacement data type for ISO7 is
+    /// UTF8.
+    Iso7(String),
+
+    /// MISP standards use UTF8 for character strings because it expands as
+    /// necessary to support alternate languages in support of NATO countries.
+    Utf8(String),
+
+    /// WARN: Older MISP standards have used UTF16, but all document updates and
+    /// new publications will utilize UTF8 instead of UTF16.
     Utf16,
-    Enumeration,
+
+    /// An unsigned integer whose value maps to a predefined table of choices. A
+    /// controlling document (e.g., standard) defines the range of allowed
+    /// unsigned integer values beginning at zero to some maximum value along
+    /// with the meanings for each value. An enumeration compacts a list of
+    /// choices into a single unsigned integer value, thereby saving bytes.
+    Enumeration(u128),
+
     FloatingPoint,
 
     /// The IMAP type is an unsigned integer, which is a mapping to a
@@ -27,8 +60,8 @@ pub enum SimpleDataType {
     /// floating-point value
     IMAP,
 
-    Integer,
-    UnsignedInteger,
+    SignedInteger(SignedInteger),
+    UnsignedInteger(UnsignedInteger),
 }
 
 #[derive(Debug, strum::EnumTryAs, thiserror::Error)]
